a9b09ee237cd10eafe028df27e64d5112f6d839d
aligerald <ali.gerald_1020@hotmail.com>
2022-06-16 14:55:35 -0500

    Nuevo titulo

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ require_once "MethodsCrud.php";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Crud</title>
+    <title>Crud Poo PHP</title>
 </head>
 
 <body>
